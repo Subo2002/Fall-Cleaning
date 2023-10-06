5ead793c55ddb772cbc9a41d8f34de1482d672a2
Snow <cypriane81@gmail.com>
2023-10-06 19:54:28 +0100

    New rake

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,24 +1,22 @@
 extends CharacterBody2D
 
+class_name Player
+
 var speed = 100.0
 
-var rake: AnimatableBody2D
-
-var raking = false
+var rake: Rake
 
 func _ready():
-	rake = $Rake
-	rake.collision_layer = 0
-	rake.visible = false
+	rake = get_tree().get_first_node_in_group("Rake")
 
 func _physics_process(_delta):
 	
 	var move = Vector2.ZERO
 
 	if Input.is_action_just_pressed("Rake"):
-		_rake_action()
+		rake._rake_action()
 
-	if raking: #prevents movement while raking
+	if rake.raking: #prevents movement while raking
 		return
 
 	if Input.is_action_pressed("Sprint"):
@@ -42,21 +40,3 @@ func _physics_process(_delta):
 	velocity.y *= 0.5
 
 	move_and_slide()
-
-	rake.global_position = position + Vector2(0, 60)
-
-func _rake_action():
-	raking = true
-	rake.visible = true
-	rake.collision_layer = 1
-
-	rake.global_position = position + Vector2(0, 70)
-
-	var tween = create_tween()
-	tween.tween_property(rake, "global_position", position + Vector2(0, 30), 1).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
-	tween.tween_callback(Callable(_end_rake_action))
-
-func _end_rake_action():
-	raking = false
-	rake.visible = false
-	rake.collision_layer = 0
